5f6ff528d9f45ac1ac47995eb8ec39ac0c9862ed
chanasit <kai.chr@hotmail.com>
2018-04-05 23:26:47 +0700

    add mtplot

--- spark_session.py
+++ spark_session.py
@@ -3,6 +3,10 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('wordCount').getOrCreate()
 
 from pyspark.sql.functions import lower, split, explode, substring, count
+from matplotlib.ticker import MaxNLocator
+
+import matplotlib.pyplot as plt
+
 
 
 # Create DataFrame representing the stream of input lines from connection to localhost:5555
@@ -26,12 +30,33 @@ query = count_hashtags_order.writeStream.outputMode("complete").format("memory")
 
 
 # 
-query.awaitTermination(timeout=600)
+query.awaitTermination(timeout=60)
 
-result = spark.sql("select * from topTagsTable").limit(5).toPandas()
+result = spark.sql("select * from topTagsTable").limit(10)
 
 print('result:', result)
 
-# 
 query.stop()
 
+top_hashtags = result.toPandas()
+
+bnk48_count = top_hashtags[top_hashtags['hashtag']=='#bnk48']['count'].values
+
+fig, ax = plt.subplots(1,1,figsize=(10,6)) 
+
+top_hashtags[top_hashtags['hashtag']!='#bnk48'].plot(kind='bar', x='hashtag', y='count', legend=False, ax=ax)
+
+ax.set_title("Top 10 hashtags related to #BNK48 (%d counts)" % bnk48_count, fontsize=18)
+
+ax.set_xlabel("Hashtag", fontsize=18)
+
+ax.set_ylabel("Count", fontsize=18)
+
+ax.set_xticklabels(ax.get_xticklabels(), {"fontsize":14}, rotation=30)
+
+ax.yaxis.set_major_locator(MaxNLocator(integer=True)) # show only integer yticks
+
+plt.yticks(fontsize=14)
+
+# show graph ploted
+plt.show()
